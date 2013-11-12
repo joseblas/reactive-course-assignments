32f5c922b0100d529f680c54a2080ffc0ec08d09
Crockeo <crockeo@gmail.com>
2013-11-11 21:25:51 -0500

    Uploading week2 (unfinished)

---
+++ Week2/src/test/scala/simulations/CircuitSuite.scala
@@ -0,0 +1,74 @@
+package simulations
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CircuitSuite extends CircuitSimulator with FunSuite {
+  val InverterDelay = 1
+  val AndGateDelay = 3
+  val OrGateDelay = 5
+  
+  test("andGate example") {
+    val in1, in2, out = new Wire
+    andGate(in1, in2, out)
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+    
+    assert(out.getSignal === false, "and 1")
+
+    in1.setSignal(true)
+    run
+    
+    assert(out.getSignal === false, "and 2")
+
+    in2.setSignal(true)
+    run
+    
+    assert(out.getSignal === true, "and 3")
+  }
+
+  //
+  // to complete with tests for orGate, demux, ...
+  //
+
+  test("orGate") {
+    val in1, in2, out = new Wire
+    
+    orGate(in1, in2, out)
+    
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+    assert(out.getSignal === false, "or 1")
+    
+    in1.setSignal(true)
+    run
+    assert(out.getSignal === true, "or 2")
+    
+    in2.setSignal(true)
+    run
+    assert(out.getSignal === true, "or 3")
+    
+    in1.setSignal(false)
+    run
+    assert(out.getSignal === true, "or 4")
+  }
+  
+  test("demux") {
+    def makeWireList(n: Int): List[Wire] =
+      if (n == 0) List()
+      else        List(new Wire) ++ makeWireList(n - 1)
+    
+    // Making sure the wire test works
+    (0 to 10).toList.foreach(a => assert(makeWireList(a).length === a, "wireListTest"))
+      
+    val in = new Wire
+    val cs, os = makeWireList(5)
+    
+    // TODO: Implement demux test
+  }
+}
