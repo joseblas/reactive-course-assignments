9d7e31349de341526dc8901bc4bb85c2348a80cb
Cerek Hillen <crockeo@gmail.com>
2013-12-13 18:40:10 -0500

    Updated again, just making sure it sends out all the messages it received while trying to resend the original Replicate

--- Week6/src/main/scala/kvstore/Replicator.scala
+++ Week6/src/main/scala/kvstore/Replicator.scala
@@ -8,6 +8,7 @@ import scala.concurrent.duration._
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
   case class Replicated(key: String, id: Long)
+  case class Resend(r: Replicate)
   
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
   case class SnapshotAck(key: String, seq: Long)
@@ -23,6 +24,8 @@ class Replicator(val replica: ActorRef) extends Actor {
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
+  var queued = List.empty[Any]
+  
   var _seqCounter = 0L
   def nextSeq = {
     val ret = _seqCounter
@@ -36,16 +39,21 @@ class Replicator(val replica: ActorRef) extends Actor {
   def normal: Receive = {
     case r: Replicate => {
       context.become(sending)
-      self ! r
+      self ! Resend(r)
     }
   }
   
   def sending: Receive = {
-    case r@Replicate(key, value, seq) => {
+    case r@Resend(Replicate(key, value, seq)) => {
       replica ! Snapshot(key, value, seq)
       Utils.delayFuture(50 milliseconds) onComplete { case _ => self ! r }
     }
     
-    case s: SnapshotAck => context.become(normal)
+    case s: SnapshotAck => {
+      context.become(normal)
+      queued.foreach(self ! _)
+    }
+    
+    case a: Any => queued = queued :+ a
   }
 }
